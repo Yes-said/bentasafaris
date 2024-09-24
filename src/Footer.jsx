c398742f741de814c25b6fc7475bf364e0da50f5
Yes-said <bobsaid358@gmail.com>
2024-09-24 16:41:08 +0300

    Footer.jsx updated

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,12 +1,12 @@
 export default function Footer() {
-    return(
-        <div>
-            <footer className="bg-gray-800 py-6">
-        <div className="container mx-auto px-6 flex flex-col md:flex-row justify-between items-center text-white">
-          <p>&copy; 2024 Benta Safaris & Adventures Ltd. All rights reserved.</p>
-          <p>Developed and maintained by Said.</p>
-        </div>
-      </footer>
-        </div>
-    );
+  return(
+      <div>
+          <footer className="bg-gray-800 py-6">
+              <div className="container mx-auto px-6 flex flex-col md:flex-row justify-between items-center text-white space-y-4 md:space-y-0">
+                  <p className="text-center md:text-left">&copy; 2024 Benta Safaris & Adventures Ltd. All rights reserved.</p>
+                  <p className="text-center md:text-right">Developed and maintained by Said.</p>
+              </div>
+          </footer>
+      </div>
+  );
 }
